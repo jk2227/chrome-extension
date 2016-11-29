f5bddf692534ca13a01f6b925de3002d6d9260ad
Ji Hun Kim <jk2227@cornell.edu>
2016-11-29 01:18:19 -0500

    made simple extension to make text bubble for highlighted text

---
+++ contentScript.js
@@ -0,0 +1,32 @@
+console.log("context script running")
+/*
+http://stackoverflow.com/questions/4409378/text-selection-and-bubble-overlay-as-chrome-extension/*
+*/
+
+// Add bubble to the top of the page.
+var bubbleDOM = document.createElement('div');
+bubbleDOM.setAttribute('class', 'selection_bubble');
+document.body.appendChild(bubbleDOM);
+
+// Lets listen to mouseup DOM events.
+document.addEventListener('mouseup', function (e) {
+  var selection = window.getSelection().toString();
+  if (selection.length > 0) {
+    renderBubble(e.clientX, e.clientY, selection);
+  }
+}, false);
+
+
+// Close the bubble when we click on the screen.
+document.addEventListener('mousedown', function (e) {
+  bubbleDOM.style.visibility = 'hidden';
+}, false);
+
+// Move that bubble to the appropriate location.
+function renderBubble(mouseX, mouseY, selection) { 
+  console.log(selection);
+  bubbleDOM.innerHTML = selection;
+  bubbleDOM.style.top = mouseY + 'px';
+  bubbleDOM.style.left = mouseX + 'px';
+  bubbleDOM.style.visibility = 'visible';
+}
