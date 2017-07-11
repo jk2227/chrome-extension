87e16880cc16ffadb1f35977835a982e40b60b5b
Ji Hun Kim <jk2227@cornell.edu>
2017-07-11 12:04:14 -0400

    changes in structure with callbacks, passing around jrecinterface as strings

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,3 @@
-
 // when clicking on the chrome extension,
 // listener helps you navigate to 
 // http://www3.nhk.or.jp/news/easy/
@@ -6,9 +5,11 @@
 // the you will be alerted so 
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
   if (!hasStarted()) {
-    window.location.href = 'http://www3.nhk.or.jp/news/easy/'
+    chrome.storage.local.set({'sequence_id': 0}, function() {
+      window.location.href = 'http://www3.nhk.or.jp/news/easy/';
+    });
   } else {
-    alert("You have already started!");
+    alert("already started!");
   }
 });
 
@@ -17,21 +18,82 @@ var EBURL = "http://ec2-54-89-85-115.compute-1.amazonaws.com:5000"
 
 $(document).ready(function() {
     if (hasStarted()) {
-      if (getCurrentDocId() == "easy") {
-        var response = getId(); 
-        navigate(response["doc_id"])
+      if (justStarted()) {
+        chrome.storage.local.get(null, function(obj) {
+          $.ajax({
+            type: 'GET',
+            url: EBURL + '/initialize',
+            success: function (e) {
+              var valuesToSet = {
+                'sequence_id': 0, 
+                'session_id': e['session_id'] 
+              }; 
+              if (obj['user_id'] == null) {
+                valuesToSet['user_id'] = e['user_id'];
+                valuesToSet['jrec'] = e['jrec'];
+                // valuesToSet['doc_id'] = e['doc_id'];
+                // valuesToSet['text'] = e['text'];
+              } 
+              chrome.storage.local.set(valuesToSet, function() {
+                $.ajax({
+                 type: 'POST',
+                 contentType: 'application/json',
+                 processData: false, 
+                 traditional: true,
+                 data: e['jrec'],
+                 url: EBURL + '/get_request_id/',
+                 success: function (id) {
+                    navigate(id);
+                  },
+                  error: function(error) {
+                    console.log(error);
+                  }
+                });
+              });
+            },
+            error: function(error) {
+              console.log(error);
+            }
+          });
+        });
       } else {
-        checkCookie();
-        var response = getText(); 
-        executeScript(response);
-        $(window).scrollTo($("#myPopup"));
-        // $(function() {
-        //   $('html,body').animate({
-        //     scrollTop: $("#myPopup").offset().top },
-        //     'slow');
-        // });
+        chrome.storage.local.get(null, function(e) {
+          $.ajax({
+                 type: 'POST',
+                 contentType: 'application/json',
+                 processData: false, 
+                 traditional: true,
+                 data: e['jrec'],
+                 url: EBURL + '/get_request_text/',
+                 success: function (text) {
+                    addTagToText(text);
+                    document.body.style.background = "black";
+                    $(".contentWrap").css('background', 'unset');
+                    $("#enq_answer_disp").css('background', 'unset');
+                    $("#enq_ansbak").css('background', 'unset');
+                    $("#side").append("<div class='overlay'></div>");
+                    $("#targetText").append("<span class='popuptext' id='myPopup'> <i> Do you understand this passage? </i> <br> <button type='button' class='yesButton' id='yesButtonId'> Yes </button> <button type='button' class='noButton' id='noButtonId'> No </button> </span>");
+                    
+                    document.getElementById("yesButtonId").addEventListener("click", function() {
+                      submitAnswerAndGetNext(true)
+                    }, false);
+                    document.getElementById("noButtonId").addEventListener("click", function() {
+                      submitAnswerAndGetNext(false)
+                    }, false);
+
+                    $('.overlay').fadeIn(300);
+                    $('html,body').animate({
+                      scrollTop: $("#myPopup").offset().top - 100},
+                      'slow');
+                  },
+                  error: function(error) {
+                    console.log(error);
+                  }
+                });
+
+        });
       }
-    }
+    } 
 });
 
 // checks whether URL contains http://www3.nhk.or.jp/
@@ -39,91 +101,17 @@ function hasStarted() {
   return window.location.href.indexOf("http://www3.nhk.or.jp/") > -1; 
 }
 
-// given a docId, navigates page to article corresponding to it
-function navigate(docId) {
-  window.location.href = 'http://www3.nhk.or.jp/news/easy/' + docId + '/' + docId + '.html'
-}
-
-// submit user response (true or false) to the backend
-// and get the next document id 
-function submitAnswerAndGetNext(userResponse) {
-  checkCookie();
-
-  var xhr2 = new XMLHttpRequest();
-  xhr2.open("POST", EBURL + "/response/" + getCookie("elanduid2") + "/" +getCurrentDocId() + "/" + userResponse.toString(), false);
-  xhr2.send();
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", EBURL + "/doc_id/" + getCookie("elanduid2") + "/", false);
-  xhr.send();
-  var nextDocId = xhr.responseText; 
-  setCookie("nextDocId", nextDocId, 60)
-  navigate(nextDocId.slice(0, 15))
-}
-
 // gets the id of the current document of interest
 // if you are not on a document but on NHK 
 // then this method will return 'easy'
-function getCurrentDocId() {
+function justStarted() {
   var currentUrl = window.location.href.split('/')
-  if (currentUrl[currentUrl.length - 2] == 'easy') {
-    return "easy";
-  } else {
-    return getCookie("nextDocId");
-  }
-}
-// calls backend to get the id of the URL 
-// ignore k10010718561000 for now? 
-function getId() {
-  checkCookie();
-  userId = getCookie("elanduid2"); 
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", EBURL + "/doc_id/" + userId + "/", false);
-  xhr.send();
-  var nextDocId = xhr.responseText; 
-  setCookie("nextDocId", nextDocId, 60)
-  return {"doc_id": nextDocId.slice(0, 15) };
+  return currentUrl[currentUrl.length - 2] == 'easy'
 }
 
-// pings the backend to retrieve the 
-// text corresponding to the current document
-function getText() {
-  var docId = getCurrentDocId(); 
-
-  var xhr = new XMLHttpRequest();
-
-  console.log(EBURL + "/text/"+ docId +"/");
-  xhr.open("GET", EBURL + "/text/"+ docId +"/", false);
-  xhr.send();
-  var text = xhr.responseText; 
-  return {"text": text};
-}
-
-// this will vary depending on the site's structure
-// blacks out everything except for the text of interest
-function executeScript(response) {
-  $(document).ready(function() {
-    document.body.style.background = "black";
-    $(".contentWrap").css('background', 'unset');
-    $("#enq_answer_disp").css('background', 'unset');
-    $("#enq_ansbak").css('background', 'unset');
-    targetText = response["text"]; 
-    addTagToText(targetText);
-    $("#side").append("<div class='overlay'></div>");
-    $("#targetText").append("<span class='popuptext' id='myPopup'> <i> Do you understand this passage? </i> <br> <button type='button' class='yesButton' id='yesButtonId'> Yes </button> <button type='button' class='noButton' id='noButtonId'> No </button> </span>");
-    
-    document.getElementById("yesButtonId").addEventListener("click", function() {
-      submitAnswerAndGetNext(true)
-    }, false);
-    document.getElementById("noButtonId").addEventListener("click", function() {
-      submitAnswerAndGetNext(false)
-    }, false);
-
-    $('.overlay').fadeIn(300);
-    $('html,body').animate({
-        scrollTop: $("#myPopup").offset().top - 100},
-        'slow');
-  });
+// given a docId, navigates page to article corresponding to it
+function navigate(docId) {
+  window.location.href = 'http://www3.nhk.or.jp/news/easy/' + docId + '/' + docId + '.html'
 }
 
 // given a text, adds the text in a div with 
@@ -200,45 +188,40 @@ function addTagToText(text) {
   $("#newsarticle").html(newHtml);
 }
 
-// given the name and value of the cookie,
-// we set the document cookie where the name 
-// corresponds to the value for exdays number of days
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+// submit user response (true or false) to the backend
+// and get the next document id 
+function submitAnswerAndGetNext(userResponse) {
+  chrome.storage.local.get(null, function(e) {
+          $.ajax({
+                 type: 'POST',
+                 contentType: 'application/json',
+                 processData: false, 
+                 traditional: true,
+                 data: e['jrec'],
+                 url: EBURL + '/record_response/' + userResponse,
+                 success: function (jrec) {
+                      chrome.storage.local.set({ 'jrec': jrec['jrec'] }, function() {
+                      $.ajax({
+                       type: 'POST',
+                       contentType: 'application/json',
+                       processData: false, 
+                       traditional: true,
+                       data: jrec['jrec'],
+                       url: EBURL + '/get_request_id/',
+                       success: function (id) {
+                          navigate(id);
+                        },
+                        error: function(error) {
+                          console.log(error);
+                        }
+                      });
+                    });
+                    
+                },
+                  error: function(error) {
+                    console.log(error);
+                  }
+                });
 
-// given a cname, get the corresponding value stored
-// in the cookie
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-// checks whether 'elanduid2' is a key in your cookies
-// if not pings the backend to generate a UUID for the 
-// user, and stores that as the value for 'elanduid2' 
-// in the cookie
-function checkCookie() {
-    var userid = getCookie("elanduid2");
-    if (userid == "") {
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", EBURL + "/get_uuid/", false);
-      xhr.send();
-      var uuid = xhr.responseText;
-      setCookie("elanduid2", uuid, 60);
-    }
+        });
 }
