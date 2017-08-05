f549d7dd3bafe6929cea1592daacbcc34c30f7cf
forsona14 <sonatithew@gmail.com>
2017-08-05 15:44:49 -0400

    Added welcome_page (still needs to be incoporated), slightly modified final_page.

---
+++ welcome_page.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById("agree").onclick = function(e) {
+		e.preventDefault(); // Prevent submission
+		alert("started");   // To Ji Hun: Start recommendation here!
+	};   
+});
