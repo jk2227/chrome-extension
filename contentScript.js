48b2dcc0031219be5d183299ffae864d9b5fa8e6
Ji Hun Kim <jk2227@cornell.edu>
2017-08-09 00:40:26 -0400

    fixing scrolling issue

--- contentScript.js
+++ contentScript.js
@@ -20,6 +20,8 @@ function convertDocIdToId(doc_id) {
 
 var storage_keys = ['user_id', 'jrec', 'sequence_id', 'session_id', 'doc_id', 'text', 'info']
 
+var POPUPONSCREEN = false; 
+
 $(document).ready(function() {
     if (hasStarted()) {
       if (justStarted()) {
@@ -53,8 +55,8 @@ $(document).ready(function() {
       } else {
         chrome.storage.local.get(storage_keys, function(e) {
           text = e['text'];
-          setTimeout(function(){}, 1000);
           addTagToText(text);
+
           document.body.style.background = "black";
           $(".contentWrap").css('background', 'unset');
           $("#side").append("<div class='overlay'></div>");
@@ -74,13 +76,26 @@ $(document).ready(function() {
           }, false);
 
           $('.overlay').fadeIn(300);
-          $('html,body').animate({
-            scrollTop: $("#myPopup").offset().top - 100},
-          'slow');
+          scrollToMyPopUp();
         });
       }
     } 
 });
+
+
+function scrollToMyPopUp() {
+  console.log("hello");
+    $('html,body').animate({
+            scrollTop: $("#myPopup").offset().top - 100
+          },
+          'slow', function() {
+            setTimeout(function(){}, 2000);
+            POPUPONSCREEN = $("#myPopup").isOnScreen();
+            if (!POPUPONSCREEN) {
+              scrollToMyPopUp();
+            }
+          });
+} 
 
 // checks whether URL contains http://www3.nhk.or.jp/
 function hasStarted() {
@@ -239,3 +254,49 @@ function submitAnswerAndGetNext(userResponse, seen) {
 
         });
 }
+
+// credits to https://stackoverflow.com/questions/8897289/how-to-check-if-an-element-is-off-screen 
+$.fn.isOnScreen = function(partial){
+
+    //let's be sure we're checking only one element (in case function is called on set)
+    var t = $(this).first();
+
+    //we're using getBoundingClientRect to get position of element relative to viewport
+    //so we dont need to care about scroll position
+    var box = t[0].getBoundingClientRect();
+
+    //let's save window size
+    var win = {
+        h : $(window).height(),
+        w : $(window).width()
+    };
+
+    //now we check against edges of element
+
+    //firstly we check one axis
+    //for example we check if left edge of element is between left and right edge of scree (still might be above/below)
+    var topEdgeInRange = box.top >= 0 && box.top <= win.h;
+    var bottomEdgeInRange = box.bottom >= 0 && box.bottom <= win.h;
+
+    var leftEdgeInRange = box.left >= 0 && box.left <= win.w;
+    var rightEdgeInRange = box.right >= 0 && box.right <= win.w;
+
+
+    //here we check if element is bigger then window and 'covers' the screen in given axis
+    var coverScreenHorizontally = box.left <= 0 && box.right >= win.w;
+    var coverScreenVertically = box.top <= 0 && box.bottom >= win.h;
+
+    //now we check 2nd axis
+    var topEdgeInScreen = topEdgeInRange && ( leftEdgeInRange || rightEdgeInRange || coverScreenHorizontally );
+    var bottomEdgeInScreen = bottomEdgeInRange && ( leftEdgeInRange || rightEdgeInRange || coverScreenHorizontally );
+
+    var leftEdgeInScreen = leftEdgeInRange && ( topEdgeInRange || bottomEdgeInRange || coverScreenVertically );
+    var rightEdgeInScreen = rightEdgeInRange && ( topEdgeInRange || bottomEdgeInRange || coverScreenVertically );
+
+    //now knowing presence of each edge on screen, we check if element is partially or entirely present on screen
+    var isPartiallyOnScreen = topEdgeInScreen || bottomEdgeInScreen || leftEdgeInScreen || rightEdgeInScreen;
+    var isEntirelyOnScreen = topEdgeInScreen && bottomEdgeInScreen && leftEdgeInScreen && rightEdgeInScreen;
+
+    return partial ? isPartiallyOnScreen : isEntirelyOnScreen;
+
+};
